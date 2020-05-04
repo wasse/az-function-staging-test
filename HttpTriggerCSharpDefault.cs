18de25efda7479665edfa75c94e121657f45f7e9
Mikel Wassenius <mikel.wassenius@vinnter.se>
2020-05-04 13:47:10 +0200

    Initial commit, first deployment

---
+++ HttpTriggerCSharpDefault.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace stagingtest
+{
+    public static class HttpTriggerCSharpDefault
+    {
+        [FunctionName("HttpTriggerCSharpDefault")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseMessage = "First deployment";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
